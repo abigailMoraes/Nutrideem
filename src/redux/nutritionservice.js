8d733b8865ba7418837660d747e4052ee0c80df3
l8k1b <abigail.theresa.moraes@gmail.com>
2023-07-17 13:48:35 -0700

    added new backend API url to client code

--- src/redux/nutritionservice.js
+++ src/redux/nutritionservice.js
@@ -1,12 +1,14 @@
+const APIURL = "nutrideem.onrender.com";
+
 export const getNutrition = async (itemId) => {
-  const res = await fetch(`http://localhost:3000/nutrition/`, {
+  const res = await fetch(`http://${APIURL}/nutrition/`, {
     method: "GET",
   });
   return res.json();
 };
 
 export const editNutrition = async (nutritionId, nutrition) => {
-  const res = await fetch(`http://localhost:3000/nutrition/${nutritionId}`, {
+  const res = await fetch(`http://${APIURL}/nutrition/${nutritionId}`, {
     method: "PUT",
     body: JSON.stringify(nutrition),
     headers: {
--- src/redux/service.js
+++ src/redux/service.js
@@ -1,9 +1,11 @@
+const APIURL = "nutrideem.onrender.com";
+
 export const getItems = async (currentPage, pageSize, query) => {
   if (query === undefined) {
     query = "";
   }
   const res = await fetch(
-    `http://localhost:3000/items?page=${currentPage}&limit=${pageSize}&search=${query}`,
+    `http://${APIURL}/items?page=${currentPage}&limit=${pageSize}&search=${query}`,
     {
       method: "GET",
     }
@@ -12,7 +14,7 @@ export const getItems = async (currentPage, pageSize, query) => {
 };
 
 export const addItem = async (item) => {
-  const res = await fetch("http://localhost:3000/items", {
+  const res = await fetch(`http://${APIURL}/items`, {
     method: "POST",
     body: JSON.stringify(item),
     headers: {
@@ -23,13 +25,13 @@ export const addItem = async (item) => {
 };
 
 export const deleteItem = async (itemId) => {
-  await fetch(`http://localhost:3000/items/${itemId}`, {
+  await fetch(`http://${APIURL}/items/${itemId}`, {
     method: "DELETE",
   });
 };
 
 export const editItem = async (item) => {
-  const res = await fetch(`http://localhost:3000/items/${item._id}`, {
+  const res = await fetch(`http://${APIURL}/items/${item._id}`, {
     method: "PUT",
     body: JSON.stringify(item),
     headers: {
